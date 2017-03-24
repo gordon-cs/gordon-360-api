74422fac05df92fec79ae99b798acff10f79720c
superpowers11 <stephanie.powers@gordon.edu>
2017-03-24 10:44:57 -0400

    Add model for slider db view

--- Gordon360/Models/CCT_DB_MODELS.Context.cs
+++ Gordon360/Models/CCT_DB_MODELS.Context.cs
@@ -41,6 +41,7 @@ namespace Gordon360.Models
         public virtual DbSet<ACT_CLUB_DEF> ACT_CLUB_DEF { get; set; }
         public virtual DbSet<JENZ_ACT_CLUB_DEF> JENZ_ACT_CLUB_DEF { get; set; }
         public virtual DbSet<ACT_INFO> ACT_INFO { get; set; }
+        public virtual DbSet<C360_SLIDER> C360_SLIDER { get; set; }
     
         public virtual ObjectResult<ACTIVE_CLUBS_PER_SESS_ID_Result> ACTIVE_CLUBS_PER_SESS_ID(string sESS_CDE)
         {
