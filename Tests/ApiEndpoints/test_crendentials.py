1caac985239ee2cee8db3af27f7eb221b938ba47
edwardzhou12038 <edward.zhou@gordon.edu>
2019-07-11 16:55:10 -0400

    authentication change and the first test in public, real public info havent found

---
+++ Tests/ApiEndpoints/test_crendentials.py
@@ -0,0 +1,6 @@
+username = '360.studenttest'
+password = 'Gordon16'
+id_number = 999999097
+username_activity_leader = '360.facultytest'
+password_activity_leader = 'Gordon16'
+id_number_activity_leader = 999999099
