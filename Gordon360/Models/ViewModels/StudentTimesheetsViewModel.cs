439bc5c11995e1a673a2dc4900295c4ea215b1fb
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2019-11-16 17:05:09 -0500

    Create model for student timesheet info

---
+++ Gordon360/Models/ViewModels/StudentTimesheetsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gordon360.Models.ViewModels
+{
+    public class StudentTimesheetsViewModel
+    {
+        public int ID { get; set; }
+        public int ID_NUM { get; set; }
+        public int EML { get; set; }
+        public string EML_DESCRIPTION { get; set; }
+        public Nullable<System.DateTime> SHIFT_START_DATETIME { get; set; }
+        public Nullable<System.DateTime> SHIFT_END_DATETIME { get; set; }
+        public float HOURLY_RATE { get; set; }
+        public float HOURS_WORKED { get; set; }
+        public int SUPERVISOR { get; set; }
+        public int COMP_SUPERVISOR { get; set; }
+        public string STATUS { get; set }
+        public Nullable<int> SUBMITTED_TO { get; set; }
+        public string SHIFT_NOTES { get; set; }
+        public string COMMENTS { get; set; }
+        public Nullable<System.DateTime> PAY_WEEK_DATE { get; set; }
+        public Nullable<System.DateTime> PAY_PERIOD_DATE { get; set; }
+        public Nullable<int> PAY_PERIOD_ID { get; set; }
+        public string LAST_CHANGED_BY { get; set; }
+        public Nullable<System.DateTime> DATETIME_ENTERED { get; set; }
+    }
+}
