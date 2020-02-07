aa92b36334dfbaf121ce8e0e301b20073b2916da
Nathaniel Rudenberg <Nathaniel.Rudenberg@gordon.edu>
2020-02-07 17:39:27 -0500

    Add comments

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,5 @@
+REM Restore dependencies needed to build the solution
 nuget restore Gordon360.sln
+
+REM Execute MSBuild to build the solution
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild" Gordon360.sln
