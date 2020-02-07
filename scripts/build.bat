e2051d7d32ac2e135a065a050b10d55ab72a9682
Nathaniel Rudenberg <5964594+NathanielRudenberg@users.noreply.github.com>
2020-02-07 13:47:36 -0500

    Command test

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin" msbuild
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin" echo "hello world"
