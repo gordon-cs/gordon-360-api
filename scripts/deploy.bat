0f882297de23c7a4385b160351b23eaeeaaceacc
Nathaniel Rudenberg <5964594+NathanielRudenberg@users.noreply.github.com>
2020-02-19 21:30:21 -0500

    Preliminary publish command

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,1 +1,1 @@
-
+REM "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild" Gordon360.sln -p:DeployOnBuild=true -p:PublishProfile=..\travis-publish-profiles\travis-dev.pubxml
