0904bc2fdd7c382f8b070c5d8b9cfd926ece2c89
Nathaniel Rudenberg <5964594+NathanielRudenberg@users.noreply.github.com>
2020-02-19 21:44:32 -0500

    test environment variables

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,5 +1,7 @@
 REM Restore dependencies needed to build the solution
 nuget restore Gordon360.sln
 
+echo "Build directory:"
+echo %TRAVIS_BUILD_DIR
 REM Execute MSBuild to build the solution
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild" Gordon360.sln
