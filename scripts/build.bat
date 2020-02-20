52fdedf03b5bcb85a971a078147dc84db6e6bc54
Nathaniel Rudenberg <5964594+NathanielRudenberg@users.noreply.github.com>
2020-02-19 22:19:45 -0500

    Test ability to copy files

--- scripts/build.bat
+++ scripts/build.bat
@@ -6,3 +6,7 @@ nuget restore Gordon360.sln
 
 REM Execute MSBuild to build the solution
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild" Gordon360.sln
+
+echo "Testing ability to copy to remote server..."
+@ECHO off
+powershell -File copyFiles.ps1
