794c74884d3d1a4bb0cdc4b668bdfa32b6f50c96
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2020-02-28 14:36:54 -0500

    Remove echo statement

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,11 +1,11 @@
-echo "Build directory:"
-echo %TRAVIS_BUILD_DIR%
-
 REM Restore dependencies needed to build the solution
 nuget restore Gordon360.sln
 
 REM Execute MSBuild to build the solution
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild" Gordon360.sln
+
+REM Publish the solution to a local directory
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild" Gordon360.sln -p:DeployOnBuild=true -p:PublishProfile=..\travis-publish-profiles\travis-dev.pubxml
 
 echo "Testing ability to copy to remote server..."
 @ECHO off
