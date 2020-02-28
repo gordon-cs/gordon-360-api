f402dad06b743403d35218deeaaf5c42b6b194f4
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2020-02-28 14:38:25 -0500

    Test copying the published solution

--- scripts/copyFiles.ps1
+++ scripts/copyFiles.ps1
@@ -1,6 +1,7 @@
 echo $env:TEST_COPY_DESTINATION
 echo "Entering build directory..."
 cd $env:TRAVIS_BUILD_DIR
+cd VSOutput\360ApiTrain
 echo "Creating credential object..."
 $cctpass = ConvertTo-SecureString $env:DEPLOY_PASSWORD -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($env:DEPLOY_USER, $cctpass)
@@ -11,7 +12,7 @@ echo "Opening remote session..."
 try {
   $session = New-PSSession -ComputerName $env:DEPLOY_SERVER -Credential $credential -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck)
   echo "Copying files to remote destination..."
-  cp -Path .\* -Destination $env:TEST_COPY_DESTINATION -ToSession $session
+  cp -Path .\* -Destination $env:TEST_COPY_DESTINATION -ToSession $session -Recurse
   echo "Closing remote Powershell session..."
   $session | Remove-PSSession
   echo "Done"
