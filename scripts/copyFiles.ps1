a159852460c349efd1ffada6304ebb2dd85abe45
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2020-03-01 16:34:39 -0500

    Create folder backups

--- scripts/copyFiles.ps1
+++ scripts/copyFiles.ps1
@@ -12,6 +12,10 @@ try {
   $session = New-PSSession -ComputerName $env:DEPLOY_SERVER -Credential $credential -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck)
   echo "Creating copytest folder..."
   Invoke-Command -Session $session {mkdir $($using:env:TEST_COPY_DESTINATION) -Force}
+  Invoke-Command -Session $session {cd $($using:env:TEST_COPY_DESTINATION)}
+  echo "Creating backup directory..."
+  $renamedDirName = "./360ApiTrain_" + (Get-Date -Format "MM-dd-yyyy_HH-mm-ss")
+  Invoke-Command -Session $session {Rename-Item ./Gordon360Api $($using:newDirName)}
   echo "Copying files to remote destination..."
   cp -Path "VSOutput\360ApiTrain" -Destination $env:TEST_COPY_DESTINATION -ToSession $session -Recurse -Force
   echo "Closing remote Powershell session..."
