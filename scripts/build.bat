77a5705c0d07f9ca7881977073deae9e534599d6
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2020-03-01 16:41:50 -0500

    Attempt to disable echo

--- scripts/build.bat
+++ scripts/build.bat
@@ -8,5 +8,5 @@ REM Publish the solution to a local directory
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild" Gordon360.sln -p:DeployOnBuild=true -p:PublishProfile=..\travis-publish-profiles\travis-dev.pubxml
 
 echo "Testing ability to copy to remote server..."
-@ECHO off
+@echo off
 powershell -ExecutionPolicy Bypass -File ./scripts/copyFiles.ps1
