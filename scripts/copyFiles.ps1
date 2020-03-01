ba21f88f130193e1f3c677d3f046d226e5668aef
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2020-03-01 14:42:51 -0500

    Force unconditional copy

--- scripts/copyFiles.ps1
+++ scripts/copyFiles.ps1
@@ -1,7 +1,6 @@
 echo $env:TEST_COPY_DESTINATION
 echo "Entering build directory..."
 cd $env:TRAVIS_BUILD_DIR
-cd VSOutput\360ApiTrain
 echo "Creating credential object..."
 $cctpass = ConvertTo-SecureString $env:DEPLOY_PASSWORD -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($env:DEPLOY_USER, $cctpass)
@@ -12,7 +11,7 @@ echo "Opening remote session..."
 try {
   $session = New-PSSession -ComputerName $env:DEPLOY_SERVER -Credential $credential -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck)
   echo "Copying files to remote destination..."
-  cp -Path .\* -Destination $env:TEST_COPY_DESTINATION -ToSession $session -Recurse
+  cp -Path VSOutput\360ApiTrain -Destination $env:TEST_COPY_DESTINATION -ToSession $session -Recurse -Force
   echo "Closing remote Powershell session..."
   $session | Remove-PSSession
   echo "Done"
