c11aca738772d7ef623d0b53c8de8c8b2f50ed67
Jacob Bradley <jacob.bradley@gordon.edu>
2020-04-07 00:45:47 -0400

    Remake edmx

--- Gordon360/Models/StudentTimesheet_DB_Models.Designer.cs
+++ Gordon360/Models/StudentTimesheet_DB_Models.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\cct.service\Desktop\Two And a Half Seniors\360-jobs-integration-api\Gordon360\Models\StudentTimesheet_DB_Models.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\cct.service.000\Desktop\Two And a Half Seniors\360-jobs-integration-api\Gordon360\Models\StudentTimesheet_DB_Models.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
