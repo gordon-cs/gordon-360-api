beea1c39efc41ad32713c94fe70e03f32c6605d3
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2020-04-09 22:44:23 -0400

    Create real deployment script

--- scripts/copyFiles.ps1
+++ scripts/copyFiles.ps1
@@ -1,34 +1,31 @@
-echo $env:TEST_COPY_DESTINATION
-echo "Entering build directory..."
-cd $env:TRAVIS_BUILD_DIR
-echo "Creating credential object..."
+Write-Output "Entering build directory..."
+Set-Location $env:TRAVIS_BUILD_DIR
+Write-Output "Creating credential object to connect to deploy server..."
 $cctpass = ConvertTo-SecureString $env:DEPLOY_PASSWORD -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($env:DEPLOY_USER, $cctpass)
-echo "Adding deployment server to trusted hosts file..."
-$trustedHosts = (Get-Item WSMan:\localhost\Client\TrustedHosts).Value
+Write-Output "Adding deploy server to trusted hosts file..."
 Set-Item wsman:\localhost\client\trustedhosts "$env:DEPLOY_SERVER" -Force
-echo "Opening remote session..."
+Write-Output "Connecting to deploy server..."
 try {
   $session = New-PSSession -ComputerName $env:DEPLOY_SERVER -Credential $credential -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck)
-  echo "Creating copytest folder..."
-  Invoke-Command -Session $session {mkdir $($using:env:TEST_COPY_DESTINATION) -Force}
-  Invoke-Command -Session $session {cd $($using:env:TEST_COPY_DESTINATION)}
-  $apiFolderExists = (Invoke-Command -Session $session {Test-Path -Path ($using:env:TEST_COPY_DESTINATION + "\360ApiTrain") -PathType Container})
+  Write-Output "Entering deploy destination on remote server..."
+  Invoke-Command -Session $session {Set-Location $($using:env:DEPLOY_DESTINATION)}
+  $apiFolderExists = (Invoke-Command -Session $session {Test-Path -Path ($($using:env:DEPLOY_DESTINATION) + "\" + $($using:env:SITE_DIR)) -PathType Container})
   if ($apiFolderExists) {
-    echo "Creating backup directory..."
-    $renamedDirName = ($env:TEST_COPY_DESTINATION + "\360ApiTrain_backup_" + (Get-Date -Format "MM-dd-yyyy_HH-mm-ss"))
-    echo ("Backing up previous build...")
-    Invoke-Command -Session $session {mkdir $($using:renamedDirName) -Force}
-    Invoke-Command -Session $session {Copy-Item ./360ApiTrain/* -Destination $($using:renamedDirName) -Recurse -Force}
+    Write-Output "Creating backup directory..."
+    $backupDir = ($env:DEPLOY_DESTINATION + "\" + $env:SITE_DIR + "_backup_" + (Get-Date -Format "MM-dd-yyyy_HH-mm-ss"))
+    Write-Output ("Backing up previous build...")
+    Invoke-Command -Session $session {mkdir $($using:backupDir) -Force}
+    Invoke-Command -Session $session {Copy-Item ("./" + $($using:env:SITE_DIR) + "/*") -Destination $($using:backupDir) -Recurse -Force}
   }
-  echo "Copying files to remote destination..."
-  cp -Path "VSOutput\360ApiTrain" -Destination $env:TEST_COPY_DESTINATION -ToSession $session -Recurse -Force
-  echo "Closing remote Powershell session..."
+  Write-Output "Copying API files to remote destination..."
+  Copy-Item -Path "VSOutput\360ApiTrain" -Destination $env:DEPLOY_DESTINATION -ToSession $session -Recurse -Force
+  Write-Output "Closing remote Powershell session..."
   $session | Remove-PSSession
-  echo "Done"
+  Write-Output "Done"
   exit 0
 }
 catch {
-    echo "Copy failed"
+    Write-Output "Copy failed"
     exit 1
 }
