7fcd9b738e9e0623ba01799c577cf5d204f770c7
Joshua Rogers <Joshua.Rogers@gordon.edu>
2020-04-18 22:10:41 -0400

    Declare Controller for student news

---
+++ Gordon360/ApiControllers/NewsController.cs
@@ -0,0 +1,25 @@
+﻿using Gordon360.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Gordon360.Controllers.Api
+{
+    public class NewsController : ApiController
+    {
+        private INewsService _newsService;
+
+        public NewsController()
+        {
+            IUnitOfWork _unitOfWork = new UnitOfWork();
+            _newsService = new NewsService(_unitOfWork);
+        }
+
+        public NewsController(INewsService newsService)
+        {
+            _newsService = newsService;
+        }
+    }
+}
