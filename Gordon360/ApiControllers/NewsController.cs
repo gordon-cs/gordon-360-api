213e43af96abcb7aa6be0e7a5940d7d516c63c11
Joshua Rogers <Joshua.Rogers@gordon.edu>
2020-04-21 23:20:05 -0400

    Add route for getting all news for given session

--- Gordon360/ApiControllers/NewsController.cs
+++ Gordon360/ApiControllers/NewsController.cs
@@ -1,4 +1,5 @@
-﻿using Gordon360.Repositories;
+﻿using Gordon360.Exceptions.CustomExceptions;
+using Gordon360.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,16 @@ using System.Web.Http;
 
 namespace Gordon360.Controllers.Api
 {
+
+    [RoutePrefix("api/news")]
     public class NewsController : ApiController
     {
         private INewsService _newsService;
 
+        // Constructor
         public NewsController()
         {
+            // Connect to service through which data (from the database) can be accessed 
             IUnitOfWork _unitOfWork = new UnitOfWork();
             _newsService = new NewsService(_unitOfWork);
         }
@@ -20,6 +25,39 @@ namespace Gordon360.Controllers.Api
         public NewsController(INewsService newsService)
         {
             _newsService = newsService;
+        }
+
+
+        /** Get all approved student news entries for the specified session
+         * id: the identifier of a specific session
+         */
+        [HttpGet]
+        [Route("session/{id}")]
+        public IHttpActionResult GetNewsForSession(string id)
+        {
+            if (!ModelState.IsValid || string.IsNullOrWhiteSpace(id))
+            {
+                string errors = "";
+                foreach (var modelstate in ModelState.Values)
+                {
+                    foreach (var error in modelstate.Errors)
+                    {
+                        errors += "|" + error.ErrorMessage + "|" + error.Exception;
+                    }
+
+                }
+                throw new BadInputException() { ExceptionMessage = errors };
+            }
+
+            var result = _activityService.GetNewsForSession(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+
         }
     }
 }
