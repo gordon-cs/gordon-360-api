f4b27111a0e1335a20944ed9626c94b295c8c0b0
Cameron Abbot <cpabbot1@gmail.com>
2020-06-12 15:45:01 -0400

    regenerating MyGordon edmx files (3)

--- Gordon360/Models/MyGordon_DB_Models.Context.cs
+++ Gordon360/Models/MyGordon_DB_Models.Context.cs
@@ -15,10 +15,10 @@ namespace Gordon360.Models
     using System.Data.Entity.Core.Objects;
     using System.Linq;
     
-    public partial class Entities : DbContext
+    public partial class MyGordonEntities : DbContext
     {
-        public Entities()
-            : base("name=Entities")
+        public MyGordonEntities()
+            : base("name=MyGordonEntities")
         {
         }
     
