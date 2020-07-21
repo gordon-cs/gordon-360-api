82e5b3c1ce6d4c3c09271bdcde0cdde104b918e8
Russ Tuck <russ.tuck@gordon.edu>
2020-07-21 14:44:13 -0400

    Added debugging output to Travis scripts

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -2,6 +2,9 @@ echo "Building application for deployment..."
 set pubProfile = "..\travis-publish-profiles\%PUB_PROFILE%"
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild" Gordon360.sln -p:DeployOnBuild=true -p:PublishProfile=%pubProfile%
 
+echo "pubProfile is: "
+echo $pubProfile
+echo ${PUB_PROFILE}
 echo "Preparing to copy API to remote server..."
 @echo off
 powershell -ExecutionPolicy Bypass -File ./scripts/copyFiles.ps1
