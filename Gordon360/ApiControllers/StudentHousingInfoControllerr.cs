cfd8639967bc379ffe0d534c10ddd1f2ea96b7d2
Joshua Rogers <Joshua.Rogers@gordon.edu>
2020-10-07 22:39:43 -0400

    Add service, view model, and controller for housing

---
+++ Gordon360/ApiControllers/StudentHousingInfoControllerr.cs
@@ -0,0 +1,32 @@
+﻿using Gordon360.Exceptions.CustomExceptions;
+using Gordon360.Repositories;
+using Gordon360.Services;
+using Gordon360.Models;
+using System.Linq;
+using System.Web.Http;
+using System.Security.Claims;
+using Gordon360.Exceptions.ExceptionFilters;
+using Gordon360.AuthorizationFilters;
+using Gordon360.Static.Names;
+using Gordon360.Models.ViewModels;
+
+namespace Gordon360.Controllers.Api
+{
+    [RoutePrefix("api/ApartmentApp")] 
+    [Authorize]
+    [CustomExceptionFilter]
+    public class StudentHousingInfoController : ApiController
+    {
+        private IStudentHousingInfoService _studentHousingInfoService;
+
+        /** Call the service that gets all student housing information
+         */
+        [HttpGet]
+        [Route("End-to-End")] //TODO follow convention of lowercase
+        public IHttpActionResult GetStudentHousingInfo()
+        {
+            var result = _studentHousingInfoService.Get();
+            return Ok(result);
+        }
+    }
+}
---
+++ Gordon360/Models/ViewModels/StudentHousingInfoViewModel.cs
@@ -0,0 +1,43 @@
+﻿/********************************
+ * This file is manually created and may be edited
+ * The view model allows access to all of the model's data without
+ * anything unnecessary. It prevents a self-referencing loop error
+ * in models that need a category that need a model etc.
+ * The implicit operator allows conversion between the model and the view model
+ ********************************/
+
+using System;
+
+namespace Gordon360.Models.ViewModels
+{
+    public class StudentHousingInfoViewModel
+    {
+        public int ID { get; set; } //TODO change type to string, since this is type used in studentProfileViewModel
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char OnOffCampus { get; set; } //TODO change type to string, since this is type used in studentProfileViewModel
+        public string BuildingDescription { get; set; }
+        public string OnCampusRoom { get; set; }
+        public char Gender { get; set; } //TODO change type to string, since this is type used in studentProfileViewModel
+        public string Email { get; set; }
+       
+        public static implicit operator StudentHousingInfoViewModel(HousingInfo h)
+        {
+            StudentHousingInfoViewModel vm = new StudentHousingInfoViewModel
+            {
+                ID = h.ID,
+                Title = h.Title,
+                FirstName = h.FirstName,
+                LastName = h.LastName,
+                OnOffCampus = h.OnOffCampus,
+                BuildingDescription = h.BuildingDescriptionID,
+                OnCampusRoom = h.OnCampusRoom,
+                Gender = h.Gender,
+                Email = h.Email,
+            };
+
+            return vm;
+        }
+    }
+}
