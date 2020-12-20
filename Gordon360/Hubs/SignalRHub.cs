3b881bfb8c7fc5ca70bd48b6113916ffb30763e4
Ariarley Dospassos <Ari.Dospassos@gordon.edu>
2020-12-20 18:09:42 -0500

    added signalR chat hub

---
+++ Gordon360/Hubs/SignalRHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Gordon360;
+
+namespace Gordon360.Hubs
+{
+    public class ChatHub : Hub
+    {
+       public async Task refreshMessages(string user)
+         {
+                await Clients.All.SendAsync("ReceivedMessagerefresh", user);
+
+         }
+
+        public async Task test()
+        {
+            await Clients.All.SendAsync("It's working boss");
+
+        }
+    }
+ 
+
+}
