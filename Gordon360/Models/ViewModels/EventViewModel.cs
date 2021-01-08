fb7547e20077f8cd059928134668a4799e10e323
EjPlatzer <Evan.Platzer@gmail.com>
2021-01-08 11:31:52 -0500

    Store all categories and requirements
    This commit updates the EventViewModel class so that it saves all categories and requirements, rather than saving a maximum of one each. The intent behind this change is to make filtering simpler and more fine-grained now that event_types are no longer sufficient.

--- Gordon360/Models/ViewModels/EventViewModel.cs
+++ Gordon360/Models/ViewModels/EventViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Xml.Linq;
+using System.Linq;
 using Gordon360.Models.ViewModels.ExtensionMethods;
 
 
@@ -15,7 +16,9 @@ namespace Gordon360.Models.ViewModels
         public string Event_Title { get; set; }
         public string Event_Type_Name { get; set; }
         public string Category_Id { get; set; }
+        public List<EventCategory> Categories { get; set; }
         public string Requirement_Id { get; set; }
+        public List<EventRequirement> Requirements { get; set; }
         public string Description { get; set; }
         public List<Object[]> Occurrences { get; set; }
         public string Organization { get; set; }
@@ -30,21 +33,39 @@ namespace Gordon360.Models.ViewModels
             Event_Name = a.Element(r25 + "event_name").ElementValueNull().Value;
             Event_Title = a.Element(r25 + "event_title").ElementValueNull().Value;
             Event_Type_Name = a.Element(r25 + "event_type_name").ElementValueNull().Value;
+
+            Categories = a.Elements(r25 + "category").Select(category => new EventCategory
+            {
+                CategoryID = category.Element(r25 + "category_id").Value,
+                CategoryName = category.Element(r25 + "category_name").Value
+            }).ToList();
+
             // Check for a category ID of 85, and if that is true, return 85
-            foreach (XElement category in a.ElementValueNull().Descendants(r25 + "category"))
-                {
-                 if (category.ElementValueNull().Element(r25 + "category_id").ElementValueNull().Value == "85")
-                    {
-                    Category_Id = "85";
-                    }
-                }
-            foreach (XElement requirement in a.ElementValueNull().Descendants(r25 + "requirement"))
-                {
-                 if (requirement.ElementValueNull().Element(r25 + "requirement_id").ElementValueNull().Value == "3")
-                    {
-                    Requirement_Id = "3";
-                    }
-                }
+
+            //foreach (XElement category in a.ElementValueNull().Descendants(r25 + "category"))
+            //    {
+            //     if (category.ElementValueNull().Element(r25 + "category_id").ElementValueNull().Value == "85")
+            //        {
+            //        Category_Id = "85";
+            //        }
+            //    }
+
+            Requirements = a.Elements(r25 + "requirement").Select(requirement => new EventRequirement
+            {
+                RequirementID = requirement.Element(r25 + "requirement_id").Value,
+                RequirementName = requirement.Element(r25 + "requirement_name").Value
+            }).ToList();
+            //(from requirement in a.Elements(r25 + "requirement") select Tuple.Create(requirement.Element(r25 + "requirement_id").Value, requirement.Element(r25 + "requirement_name").Value)).ToList();
+
+            //foreach (XElement requirement in a.ElementValueNull().Descendants(r25 + "requirement"))
+            //    {
+            //    // Requirements.Add(requirement.ElementValueNull().Element(r25 + "requirement_id").ElementValueNull().Value);
+            //     if (requirement.ElementValueNull().Element(r25 + "requirement_id").ElementValueNull().Value == "3")
+            //        {
+            //        Requirement_Id = "3";
+            //        }
+            //    }
+
             List<Object[]> placeholder = new List<object[]>();
             foreach (XElement reservation in a.Element(r25 + "profile").ElementValueNull().Descendants(r25 + "reservation"))
             {
@@ -54,13 +75,25 @@ namespace Gordon360.Models.ViewModels
                     reservation.Element(r25 + "event_end_dt").ElementValueNull().Value,
                     reservation.Element(r25 + "space_reservation").ElementValueNull().Element(r25 + "space").ElementValueNull().Element(r25 + "formal_name").ElementValueNull().Value
                 };
-                placeholder.Add(occurrence);  
+                placeholder.Add(occurrence);
             }
             Occurrences = placeholder;
             Description = a.Element(r25 + "event_text").ElementValueNull().Element(r25 + "text").ElementValueNull().Value;
             Organization = a.Element(r25 + "organization").ElementValueNull().Element(r25 + "organization_name").ElementValueNull().Value;
 
         }
+    }
+
+    public class EventCategory
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+
+    public class EventRequirement
+    {
+        public string RequirementID { get; set; }
+        public string RequirementName { get; set; }
     }
 
 
