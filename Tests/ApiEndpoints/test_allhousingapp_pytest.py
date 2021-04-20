eb63c158e2ccc8ce757be7937b430f9d8329a4f5
cct.service <cct.service@gordon.edu>
2021-04-20 14:11:20 -0400

    Remove old pytests

--- Tests/ApiEndpoints/test_allhousingapp_pytest.py
+++ Tests/ApiEndpoints/test_allhousingapp_pytest.py
@@ -12,42 +12,6 @@ class Test_AllHousingAppTest(control.testCase):
 # HOUSING APP TESTS #
 # # # # # # # # # # #
 
-#    Verify that a student can get their current information
-#    Endpoint -- api/housing/
-#    Expected Status Code -- 200 OK
-#    Expected Response Body -- A list of one dictionary with user answer
-    def test_get_all_for_apartment_app(self):
-        self.session = self.createAuthorizedSession(control.username, control.password)
-        self.url = control.hostURL + 'api/housing/apartmentInfo'
-        response = api.get(self.session, self.url)
-
-        if not response.status_code == 200:
-            pytest.fail('Expected 200 OK, got {0}.'\
-                .format(response.status_code))
-        try:
-            response.json()
-        except ValueError:
-            pytest.fail('Expected Json respone body, got {0}.'\
-                .format(response.text))
-        assert response.json()[0]["OnOffCampus"] == None
-        assert response.json()[0]["OnCampusRoom"] == '210'
-
-#    Verify that a user can submit a housing application
-#    Endpoint -- 'api/housing/putApartmentApplication'
-#    Expected Status Code -- 200 OK
-#    Expected Content --
-    def test_put_apartment_application(self):
-        self.session = self.createAuthorizedSession(control.username, control.password)
-        self.url = control.hostURL + 'api/housing/putApartmentApplication'
-        self.data = {
-            'ID': control.my_id_number
-        }
-        self.requestID = -1
-        response = api.put(self.session, self.url, self.data)
-        if not response.status_code == 200:
-            pytest.fail('Expected 200 OK, got {0}.'\
-                .format(response.status_code))
-
 #    Verify that a user who is on the admin whitelist gets the OK to access staff features
 #    Endpoint -- 'api/housing/admin'
 #    Expected Status Code -- 200 OK
@@ -121,48 +85,6 @@ class Test_AllHousingAppTest(control.testCase):
         if not response.status_code == 404:
             pytest.fail('Expected 404 Not Found, got {0}.'\
                 .format(response.status_code))
-        
 
-#   Verify that the editor (primary applicant) can save the application
-#   Endpoint -- 'api/housing/apartment/save'
-#   Expected Status Code -- 201 Created
-#   Expected Content --
-    # def test_post_save_apartment_application__editor(self):
-    #     self.session = self.createAuthorizedSession(control.username, control.password)
-    #     self.url = control.hostURL + 'api/housing/apartment/save'
-    #     self.data = {
-    #         'ID': control.my_id_number
-    #     }
-    #     self.requestID = -1
-    #     response = api.post(self.session, self.url, self.data)
 
-    #     #
-    #     #   PLACE HOLDER
-    #     #   Put pytest codes here
-    #     #
-
-    #     if not response.status_code == 201:
-    #         pytest.fail('Expected 201 Created, got {0}.'\
-    #             .format(response.status_code))
-
-#   Verify that the editor (primary applicant) can edit the application
-#   Endpoint -- 'api/housing/apartment/save'
-#   Expected Status Code -- 201 Created
-#   Expected Content --
-    def test_put_edit_apartment_application__editor(self):
-        self.session = self.createAuthorizedSession(control.username, control.password)
-        self.url = control.hostURL + 'api/housing/apartment/save'
-        self.data = {
-            'ID': control.my_id_number
-        }
-
-        response = api.put(self.session, self.url, self.data)
-        # Sudo code
-        # First, create an authorized session and check if the student is the editor
-        # If editor, call the save api to save the application. (accesses the database)
-        # No additional applicant or hall preference is added.
-
-        if not response.status_code == 201:
-            pytest.fail('Expected 201 Created, got {0}.'\
-                .format(response.status_code))
 
