3544dc8b6d791e5899138f0754b6f845e4140c9c
Joshua Rogers <joshua.a.rogers.24@gmail.com>
2021-04-27 14:46:48 -0400

    Robustify test data

--- Tests/ApiEndpoints/test_allhousingapp_pytest.py
+++ Tests/ApiEndpoints/test_allhousingapp_pytest.py
@@ -14,39 +14,79 @@ class Test_AllHousingAppTest(control.testCase):
 
     # Create test data
 
+    COMPLETE_APPLICANT1_JSON = {
+            'Profile' : {
+                'AD_Username' : control.username,
+                'Class' : 'Sophomore',
+            },
+            'OffCampusProgram' : 'Chemistry'
+        }
+
+    MINIMAL_APPLICANT1_JSON = {
+            'Profile' : {
+                'AD_Username' : control.username,
+            },
+        }
+
+    COMPLETE_APPLICANT2_JSON = {
+            'Profile' : {
+                'AD_Username' : control.leader_username,
+                'Class' : 'Senior',
+            },
+            'OffCampusProgram' : 'Center for Entrepreneurial Leadership'
+        }
+
+    MINIMAL_APPLICANT2_JSON = {
+            'Profile' : {
+                'AD_Username' : control.leader_username,
+            },
+        }
+
+    HALL1_RANK1 = {
+            'HallRank' : 1,
+            'HallName' : 'Bromley'
+        }
+
+    HALL1_RANK2 = {
+            'HallRank' : 2,
+            'HallName' : 'Bromley'
+        }
+
+    HALL2_RANK1 = {
+            'HallRank' : 1,
+            'HallName' : 'Tavilla'
+        }
+
     NEW_APPLICATION_JSON = {  
             'ApplicationID' : -1, 
-            'EditorProfile' : {
-                'AD_Username' : control.username,
-            },  
+            'EditorProfile' : MINIMAL_APPLICANT1_JSON,  
             'Applicants' : [
-                {
-                    'Profile' : {
-                       'AD_Username' : control.username,
-                       'Class' : 'Junior',
-                    },
-                },
-                {
-                    'Profile' : {
-                       'AD_Username' : control.leader_username,
-                       'Class' : 'Senior',
-                    },
-                },
+                COMPLETE_APPLICANT1_JSON,
+                COMPLETE_APPLICANT2_JSON
             ],
             'ApartmentChoices' : [
-                {
-                    'HallRank' : 1,
-                    'HallName' : 'Tavilla'
-                },
-                {
-                    'HallRank' : 2,
-                    'HallName' : 'Conrad'
-                },
-                {
-                    'HallRank' : 3,
-                    'HallName' : 'Hilton'
-                }
+                HALL2_RANK1,
+                HALL1_RANK2
             ],
+        }
+
+    NEW_APP_MINIMAL_JSON = {  
+            'ApplicationID' : -1, 
+            'EditorProfile' : MINIMAL_APPLICANT1_JSON,  
+            'Applicants' : [
+                MINIMAL_APPLICANT1_JSON,
+            ],
+            'ApartmentChoices' : [],
+        }
+
+    def create_application_json(appID="", dSubmitted="", dModified="", editorP="", applicants="", apartChoices=""):
+        return {
+            'ApplicationID' : appID,
+            'DateSubmitted' : dSubmitted,
+            'DateModified' : dModified,
+            'EditorProfile' : editorP,
+            'Applicants' : applicants,
+            'ApartmentChoices' : apartChoices
         }
 
     # TEST!
@@ -55,7 +95,7 @@ class Test_AllHousingAppTest(control.testCase):
 #    Endpoint -- 'api/housing/admin'
 #    Expected Status Code -- 200 OK
 #    Expected Content -- Empty response content
-    def test_is_on_whitelist(self):
+    def test_is_admin(self):
         self.session = self.createAuthorizedSession(control.username, control.password)
         self.url = control.hostURL + 'api/housing/admin'
         # add test user to whitelist
@@ -75,7 +115,7 @@ class Test_AllHousingAppTest(control.testCase):
 #    Endpoint -- 'api/housing/admin'
 #    Expected Status Code -- 404 Not Found
 #    Expected Content -- Empty Response content
-    def test_not_on_whitelist(self):
+    def test_not_admin(self):
         self.session = self.createAuthorizedSession(control.username, control.password)
         # the test user should not be an admin unless it is added in one of these tests
         self.url = control.hostURL + 'api/housing/admin'
@@ -105,7 +145,7 @@ class Test_AllHousingAppTest(control.testCase):
     def test_get_application_is_found(self):
         self.session = self.createAuthorizedSession(control.username, control.password)
         self.url = control.hostURL + 'api/housing/apartment/applications'
-        # the user in the context of the pytests is control.username (360.studenttest), 
+        # the user in this context is 360.studenttest because we create the authorized session with control.username, 
         # so we are creating an application with that user on it to get an OK from 'api/housing/apartment'
         self.data = NEW_APPLICATION_JSON 
         appIDResponse = api.postAsJson(self.session, self.url, self.data)
@@ -184,6 +224,27 @@ class Test_AllHousingAppTest(control.testCase):
         # clean up
         appID = appIDResponse.content
         self.url = control.hostURL + 'api/housing/apartment/applications/' + str(appID)
+        api.delete(self.session, self.url) # delete has not yet been implemented in this branch
+
+        if not appIDResponse.status_code == 200:
+            pytest.fail('Expected 200 Not Found, got {0}.'\
+                .format(response.status_code))
+
+#    Verify that an application is edited successfully when an applicant is added 
+#    Endpoint -- 'api/housing/apartment/applications/{applicationID}'
+#    Expected Status Code -- 200 OK
+#    Expected Content -- 
+    def test_application_edited_applicant(self):
+        self.session = self.createAuthorizedSession(control.username, control.password)
+        self.url = control.hostURL + 'api/housing/apartment/applications'
+        self.data = NEW_APPLICATION_JSON
+        appIDResponse = api.postAsJson(self.session, self.url, self.data)
+
+        appID = appIDResponse.content
+        self.url = control.hostURL + 'api/housing/apartment/applications/' + str(appID)
+        self.data = NEW_APPLICATION_JSON #TODO
+
+        # clean up
         api.delete(self.session, self.url) # delete has not yet been implemented in this branch
 
         if not response.status_code == 200:
