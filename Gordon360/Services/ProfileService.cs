bf2b3bae05566c87b1918d2815dc862dbef7ef63
Hyungyu Park <70544403+iamphg97@users.noreply.github.com>
2021-07-01 11:06:25 -0400

    Update Gordon360/Services/ProfileService.cs
    Co-authored-by: Cameron Abbot <cpabbot1@gmail.com>

--- Gordon360/Services/ProfileService.cs
+++ Gordon360/Services/ProfileService.cs
@@ -24,18 +24,6 @@ namespace Gordon360.Services
             _accountService = new AccountService(_unitOfWork);
         }
         /// <summary>
-        /// NOTE: Also a helper method, hence why it returns a StudentProfile model
-        /// rather than a StudentProfileViewModel
-        /// </summary>
-        /// <param name="id">username</param>
-        /// <returns>StudentProfileViewModel if found, null if not found</returns>
-        public Student GetStudentByID(string id)
-        {
-            var all = Data.StudentData;
-            return all.FirstOrDefault(x => x.ID == id);
-        }
-
-        /// <summary>
         /// get student profile info
         /// </summary>
         /// <param name="username">username</param>
