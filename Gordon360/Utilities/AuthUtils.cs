37bb7db79d5f1f3ef93a852001890b0264fc22ba
Evan Platzer <evan.platzer@gmail.com>
2022-03-16 12:26:08 -0400

    Add util method to get username from auth token

---
+++ Gordon360/Utilities/AuthUtils.cs
@@ -0,0 +1,26 @@
+﻿using Gordon360.Database.CCT;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Gordon360.Utilities
+{
+    public class AuthUtils
+    {
+        /// <summary>
+        /// Get the username of the authenticated user
+        /// </summary>
+        /// <param name="User">The ClaimsPrincipal representing the user's authentication claims</param>
+        /// <returns>Username of the authenticated user, or null if none is found</returns>
+        public static string GetAuthenticatedUserUsername(ClaimsPrincipal User)
+        {
+            return User.FindFirstValue(ClaimTypes.Upn).Split("@")[0];
+        }
+
+        //public static int? GetUserID(ClaimsPrincipal User)
+        //{
+        //    string? username = GetUsername(User);
+        //    if (username == null) return null;
+        //    return new CCTContext().ACCOUNT.Where(a => a.AD_Username == username).Select(a => int.Parse(a.gordon_id)).FirstOrDefault();
+        //}
+    }
+}
