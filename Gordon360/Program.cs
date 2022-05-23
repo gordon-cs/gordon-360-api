0c1513ad6db0f1282cc3926919c5cec16fcf7bf7
EjPlatzer <Evan.Platzer@gordon.edu>
2022-05-23 16:54:06 -0400

    Remove https redirection (not intended for APIs)
    HTTPS is supposed to be enforced for APIs by refusing non-HTTPS connections, according to Microsoft. the UseHttpsRedirection configuration breaks the API.

--- Gordon360/Program.cs
+++ Gordon360/Program.cs
@@ -53,8 +53,6 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(
