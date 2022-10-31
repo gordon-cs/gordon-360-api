70a377a1d0b83ae5a64611a30b464031b7d4df42
Amos Cha <Amos.Cha@gordon.edu>
2022-10-30 23:41:58 -0400

    Fix ServiceInterface error, not sure why it did not save

--- Gordon360/Services/ServiceInterfaces.cs
+++ Gordon360/Services/ServiceInterfaces.cs
@@ -324,7 +324,7 @@ namespace Gordon360.Services
             IEnumerable<SeriesViewModel> GetSeriesByActivityID(int activityID);
             SeriesViewModel GetSeriesByID(int seriesID);
             Task<int> PostSeries(CreateSeriesViewModel newSeries, int? referenceSeriesID);
-            Task UpdateSeries();
+            Task UpdateSeries(UpdateSeriesViewModel series);
             Task ScheduleMatches(int seriesID);
         }
 
