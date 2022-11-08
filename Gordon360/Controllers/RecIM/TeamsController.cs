b63ccc08a14023e49aa4929997bc7057f10d006d
Kaiwen Chu <kevin.chu@gordon.edu>
2022-11-08 08:58:33 -0500

    Update TeamsController.cs

--- Gordon360/Controllers/RecIM/TeamsController.cs
+++ Gordon360/Controllers/RecIM/TeamsController.cs
@@ -1,4 +1,4 @@
-﻿using Gordon360.Authorization;
+using Gordon360.Authorization;
 using Gordon360.Models.CCT;
 using Gordon360.Models.ViewModels.RecIM;
 using Gordon360.Services.RecIM;
@@ -49,7 +49,7 @@ namespace Gordon360.Controllers.RecIM
         /// <param name="newTeam"></param>
         /// <returns></returns>
         [HttpPost]
-        [Route("{teamID}")]
+        [Route("")]
         public async Task<ActionResult> CreateTeam(TeamUploadViewModel newTeam)
         {
             var username = AuthUtils.GetUsername(User);
@@ -65,63 +65,25 @@ namespace Gordon360.Controllers.RecIM
         /// <param name="teamID"></param>
         /// <returns></returns>
         [HttpPost]
-        [Route("{teamID}/users")]
+        [Route("")]
         public async Task<ActionResult> AddUserToTeam(string participantUsername, int teamID)
         {
-            var username = AuthUtils.GetUsername(User);
-
-            // only team captain is allowed to add user to team
-            if (_teamService.IsTeamCaptain(teamID, username))
-            {
-                await _teamService.AddUserToTeamAsync(teamID, participantUsername);
-                return Ok();
-            }
-
-            return Unauthorized();
-        }
-
-        /// <summary>
-        /// Update the participant role in a team
-        /// </summary>
-        /// <param name="teamID"></param>
-        /// <param name="participantID"></param>
-        /// <param name="participantRoleID"></param>
-        /// <returns></returns>
-        [HttpPut]
-        [Route("{teamID}/users/{userID}")]
-        public async Task<ActionResult> UpdateParticipantRole(int teamID, int participantID, int participantRoleID)
-        {
-            var username = AuthUtils.GetUsername(User);
-
-            // only team captain is allowed to add user to team
-            if (_teamService.IsTeamCaptain(teamID, username))
-            {
-                await _teamService.UpdateParticipantRoleAsync(teamID, participantID, participantRoleID);
-                return Ok();
-            }
-
-            return Unauthorized();
+            await _teamService.AddUserToTeamAsync(teamID, participantUsername);
+            return Ok();
         }
 
         /// <summary>
         /// Update a team info
         /// </summary>
         /// <param name="team"></param>
+        /// <param name="teamID"></param>
         /// <returns></returns>
-        [HttpPut]
+        [HttpPatch]
         [Route("{teamID}")]
-        public async Task<ActionResult> UpdateTeamInfo(TeamPatchViewModel team)
+        public async Task<ActionResult> UpdateTeamInfo(int teamID, TeamPatchViewModel team)
         {
-            var username = AuthUtils.GetUsername(User);
-
-            // only team captain is allowed to add user to team
-            if (_teamService.IsTeamCaptain(team.ID, username))
-            {
-                await _teamService.UpdateTeamAsync(team);
-                return Ok();
-            }
-
-            return Unauthorized();
+            var id = await _teamService.UpdateTeamAsync(teamID, team);
+            return Ok(id);
         }
     }
 }
