8bdb82d908eb2dc24ab6731c6261c8acd01a2287
Amos Cha <Amos.Cha@gordon.edu>
2022-11-11 21:51:50 -0500

    Add SeriesScheduleViewModel with 2 implementations

---
+++ Gordon360/Models/ViewModels/RecIM/SeriesScheduleViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Gordon360.Models.CCT;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gordon360.Models.ViewModels.RecIM
+{
+    public class SeriesScheduleViewModel
+    {
+        public int ID { get; set; }
+        public Dictionary<string,bool> AvailableDays { get; set; } = new Dictionary<string,bool>();
+        //public bool Sun { get; set; }
+        //public bool Mon { get; set; }
+        //public bool Tue { get; set; }
+        //public bool Wed { get; set; }
+        //public bool Thu { get; set; }
+        //public bool Fri { get; set; }
+        //public bool Sat { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int? EstMatchTime { get; set; }
+
+        public static implicit operator SeriesScheduleViewModel(SeriesSchedule s)
+        {
+            //trial implementation
+            return new SeriesScheduleViewModel
+            {
+                ID = s.ID,
+                AvailableDays = new Dictionary<string, bool>()
+                    {
+                        { "Sun",s.Sun },
+                        { "Mon",s.Mon },
+                        { "Tue",s.Tue },
+                        { "Wed",s.Wed },
+                        { "Thu",s.Thu },
+                        { "Fri",s.Fri },
+                        { "Sat",s.Sat },
+                    },
+                StartTime = s.StartTime,
+                EndTime = s.EndTime,
+                EstMatchTime = s.EstMatchTime
+            };
+            //return new SeriesScheduleViewModel
+            //{
+            //    ID = s.ID,
+            //    Sun = s.Sun,
+            //    Mon = s.Mon,
+            //    Tue = s.Tue,
+            //    Wed = s.Wed,
+            //    Thu = s.Thu,
+            //    Fri = s.Fri,
+            //    Sat = s.Sat,
+            //    StartTime = s.StartTime,
+            //    EndTime = s.EndTime,
+            //    EstMatchTime = s.EstMatchTime
+            //};
+        }
+    }
+}
