0e0b150412422b2785c08bde037996b3d5362dbb
amos-cha <amos.cha98@gmail.com>
2023-01-31 23:06:06 -0500

    Initialize foward facing route viewmodels

---
+++ Gordon360/Models/ViewModels/RecIM/SeriesSchedulePatchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Gordon360.Models.CCT;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gordon360.Models.ViewModels.RecIM
+{
+    public class SeriesSchedulePatchViewModel
+    {
+        public IEnumerable<ScheduleDayViewModel> AvailableDays { get; set; }
+        public DateTime? DailyStartTime { get; set; }
+        public DateTime? DailyEndTime { get; set; }
+        public int? EstMatchTime { get; set; }
+    }
+}
---
+++ Gordon360/Models/ViewModels/RecIM/SeriesScheduleUploadViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Gordon360.Models.CCT;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+
+namespace Gordon360.Models.ViewModels.RecIM
+{
+    public class SeriesScheduleUploadViewModel
+    {
+        public IEnumerable<ScheduleDayViewModel> AvailableDays { get; set; }
+        public DateTime DailyStartTime { get; set; }
+        public DateTime DailyEndTime { get; set; }
+        public int EstMatchTime { get; set; }
+    }
+}
