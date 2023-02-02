1e5bab1565896e7058546d322dbf73dfd2d55a55
Amos Cha <78386128+amos-cha@users.noreply.github.com>
2023-02-02 05:31:20 +0000

    Remove redundant check, handled: https://github.com/gordon-cs/gordon-360-api/pull/800

--- Gordon360/Services/RecIM/TeamService.cs
+++ Gordon360/Services/RecIM/TeamService.cs
@@ -270,9 +270,6 @@ namespace Gordon360.Services.RecIM
         
         public async Task<TeamViewModel> PostTeamAsync(TeamUploadViewModel t, string username)
         {
-            if (_context.Team.FirstOrDefault(team => team.Name == t.Name && team.ActivityID == t.ActivityID) is not null)
-                throw new ResourceCreationException() { ExceptionMessage = $"The name: {t.Name} has already been taken." };
-
             var team = new Team
             {
                 Name = t.Name,
@@ -339,11 +336,6 @@ namespace Gordon360.Services.RecIM
 
         public async Task<TeamViewModel> UpdateTeamAsync(int teamID, TeamPatchViewModel update)
         {
-            var teamToUpdate = _context.Team.FirstOrDefault(t => t.ID == teamID);
-            if (update.Name is not null)
-                if (_context.Team.FirstOrDefault(t => t.Name == update.Name && teamToUpdate.ActivityID == t.ActivityID) is not null)
-                    throw new ResourceCreationException() { ExceptionMessage = $"The name: {update.Name} has already been taken." };
-
             var t = await _context.Team.FindAsync(teamID);
             t.Name = update.Name ?? t.Name;
             t.StatusID = update.StatusID ?? t.StatusID;
