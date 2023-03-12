0df3fa291e1dcc6dce1444aa40c182deb00c4343
amos-cha <amos.cha98@gmail.com>
2023-03-12 17:32:17 -0400

    Add Conversion from UTC to EST

--- Gordon360/Extensions/DateTimeExtensions.cs
+++ Gordon360/Extensions/DateTimeExtensions.cs
@@ -20,7 +20,28 @@ static public class DateTimeExtensions
     {
         if (nullableDateTime is DateTime dateTime)
             return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+        return null;
+    }
 
+    /// <summary>
+    /// Converts UTC DateTime to Eastern Standard Time 
+    /// List of time zones specified by TimeZoneInfo https://i.stack.imgur.com/zHzGt.png
+    /// </summary>
+    /// <param name="dateTime">DateTime with kind UTC</param>
+    /// <returns>Converted DateTime with kind Unspecified</returns>
+    static public DateTime ConvertUtcToEst(this DateTime dateTime) => TimeZoneInfo.ConvertTimeFromUtc(dateTime, TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
+
+
+    /// <summary>
+    /// Converts UTC DateTime? to Eastern Standard Time 
+    /// List of time zones specified by TimeZoneInfo https://i.stack.imgur.com/zHzGt.png
+    /// </summary>
+    /// <param name="nullableDateTime">DateTime with kind UTC</param>
+    /// <returns>Converted DateTime with kind Unspecified or null if parameter is null</returns>
+    static public DateTime? ConvertUtcToEst(this DateTime? nullableDateTime)
+    {
+        if (nullableDateTime is DateTime dateTime)
+            return TimeZoneInfo.ConvertTimeFromUtc(dateTime, TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
         return null;
     }
 }
