27fd84972091ea62508876f0df9c1249d6bf235b
amos-cha <amos.cha98@gmail.com>
2023-04-18 03:13:22 -0400

    Add sportsmanship rating as a visual on listings

--- Gordon360/Models/ViewModels/RecIM/TeamRecordViewModel.cs
+++ Gordon360/Models/ViewModels/RecIM/TeamRecordViewModel.cs
@@ -10,6 +10,7 @@ namespace Gordon360.Models.ViewModels.RecIM
         public int WinCount { get; set; }  
         public int LossCount { get; set; }
         public int TieCount { get; set; }
+        public double? SportsmanshipRating { get; set; } // has to be nullable due to calculation query possibly propagating null
         public static implicit operator TeamRecordViewModel(SeriesTeam st)
         {
             return new TeamRecordViewModel
