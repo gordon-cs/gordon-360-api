c52b10c8d240466696d805bed1ee675f27eb997d
amos-cha <amos.cha98@gmail.com>
2023-04-28 10:36:20 -0400

    Change seriesschedule upload to timeonly

--- Gordon360/Models/ViewModels/RecIM/SeriesScheduleUploadViewModel.cs
+++ Gordon360/Models/ViewModels/RecIM/SeriesScheduleUploadViewModel.cs
@@ -19,8 +19,8 @@ namespace Gordon360.Models.ViewModels.RecIM
         public int? SeriesID { get; set; }
         public Schedule AvailableDays { get; set; }
         public IEnumerable<int> AvailableSurfaceIDs { get; set; }
-        public DateTime DailyStartTime { get; set; }
-        public DateTime DailyEndTime { get; set; }
+        public TimeOnly DailyStartTime { get; set; }
+        public TimeOnly DailyEndTime { get; set; }
         public int EstMatchTime { get; set; }
     }
 }
