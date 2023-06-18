a88c668d755910ad3fb041a3552b681a4be288c2
amos-cha <amos.cha98@gmail.com>
2023-06-18 12:15:12 -0400

    Add reporting details

--- Gordon360/Controllers/RecIM/RecIMController.cs
+++ Gordon360/Controllers/RecIM/RecIMController.cs
@@ -19,12 +19,25 @@ namespace Gordon360.Controllers.RecIM
             _recimService = recimService;
         }
 
+        /// <summary>
+        /// Rec-IM Reporting:
+        ///     Every semester Rec-IM needs to report, their own client,
+        ///     information regarding new users, retention of users, 
+        ///     gender ratio, involvements...
+        ///     
+        ///     This route does not specify semester terms, however, is more
+        ///     specific in which it can select time constraints from beginning
+        ///     to end with no limit to time in between
+        /// </summary>
+        /// <param name="startTime">report start time</param>
+        /// <param name="endTime">report end time</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("report")]
         [StateYourBusiness(operation = Operation.READ_ALL, resource = Resource.RECIM)]
-        public ActionResult<RecIMGeneralReportViewModel> GetReport(DateTime start, DateTime end)
+        public ActionResult<RecIMGeneralReportViewModel> GetReport(DateTime startTime, DateTime endTime)
         {
-            var res = _recimService.GetReport(start,end);
+            var res = _recimService.GetReport(startTime,endTime);
             return Ok(res);
         }
 
