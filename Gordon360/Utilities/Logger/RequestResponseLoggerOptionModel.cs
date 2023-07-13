86758ee4070810c4abcac84414538e1477c96bbd
amos-cha <amos.cha98@gmail.com>
2023-07-13 09:51:13 -0400

    Add logging option model

---
+++ Gordon360/Utilities/Logger/RequestResponseLoggerOptionModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Gordon360.Utilities.Logger
+{
+    public class RequestResponseLoggerOptionModel
+    {
+        public bool IsEnabled { get; set; }
+        public string Name { get; set; }
+        public string DateTimeFormat { get; set; }
+    }
+}
