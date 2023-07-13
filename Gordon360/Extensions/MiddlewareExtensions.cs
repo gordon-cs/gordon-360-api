8052eec732bfad9c2d58a8b81cf860ee6e2d1574
amos-cha <amos.cha98@gmail.com>
2023-07-13 09:54:59 -0400

    Add middleware extensions for factory logger middleware

---
+++ Gordon360/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+using Gordon360.Utilities.Logger;
+using Microsoft.AspNetCore.Builder;
+
+namespace Gordon360.Extensions
+{
+    public static class MiddlewareExtensions
+    { 
+        public static IApplicationBuilder UseFactoryActivatedMiddleware(
+            this IApplicationBuilder app)
+            => app.UseMiddleware<RequestResponseLoggerMiddleware>();
+    }
+    
+}
