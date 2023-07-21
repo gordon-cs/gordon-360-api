140a75ac81167598bed2e8d9fd6940fb28d5215b
Amos Cha <78386128+amos-cha@users.noreply.github.com>
2023-07-20 20:06:22 -0400

    Update Gordon360/Extensions/MiddlewareExtensions.cs
    Co-authored-by: Russ Tuck <russ.tuck@gordon.edu>

--- Gordon360/Extensions/MiddlewareExtensions.cs
+++ Gordon360/Extensions/MiddlewareExtensions.cs
@@ -1,7 +1,8 @@
 ﻿
 using Gordon360.Utilities.Logger;
 using Microsoft.AspNetCore.Builder;
-
+// See https://learn.microsoft.com/en-us/aspnet/core/fundamentals/middleware/extensibility?view=aspnetcore-6.0 
+// for background on how this works.
 namespace Gordon360.Extensions
 {
     public static class MiddlewareExtensions
