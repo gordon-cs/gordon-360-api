95b232df05bb93512774f8ca9e1b9e920ee9e7a8
Amos Cha <78386128+amos-cha@users.noreply.github.com>
2023-07-24 16:54:16 -0400

    Update Gordon360/Controllers/RecIM/AffiliationsController.cs

--- Gordon360/Controllers/RecIM/AffiliationsController.cs
+++ Gordon360/Controllers/RecIM/AffiliationsController.cs
@@ -12,7 +12,6 @@ using System.Linq;
 namespace Gordon360.Controllers.RecIM
 {
     [Route("api/recim/[controller]")]
-    [AllowAnonymous]
     public class AffiliationsController : GordonControllerBase
     {
         private readonly IAffiliationService _affiliationService;
