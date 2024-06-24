3839d8ae06cacb3ecfec00800e12e91f4f91551b
amos-cha <amos.cha98@gmail.com>
2024-06-24 11:21:06 -0400

    Create post route

---
+++ Gordon360/Models/ViewModels/PosterUploadViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Gordon360.Models.CCT;
+using System;
+
+namespace Gordon360.Models.ViewModels;
+
+public class PosterUploadViewModel
+
+{
+    public string ClubCode { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public string ImagePath { get; set; }
+    public DateTime VisibleDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public string Status { get; set; }
+
+}
