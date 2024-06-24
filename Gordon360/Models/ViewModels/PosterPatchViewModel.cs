53fb53713b1f1fcd438bb612e716e17f7c2fdbc2
amos-cha <amos.cha98@gmail.com>
2024-06-24 10:40:14 -0400

    Add Viewmodels

---
+++ Gordon360/Models/ViewModels/PosterPatchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Gordon360.Models.CCT;
+using System;
+
+namespace Gordon360.Models.ViewModels;
+
+public class PosterPatchViewModel
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? ImagePath { get; set; }
+    public DateTime? VisibleDate { get; set; }
+    public DateTime? ExpirationDate { get; set; }
+    public string? Status { get; set; }
+
+}
---
+++ Gordon360/Models/ViewModels/PosterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Gordon360.Models.CCT;
+using System;
+
+namespace Gordon360.Models.ViewModels;
+
+public class PosterViewModel
+{
+    public string ClubCode { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+    public DateTime VisibleDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public string Status { get; set; }
+
+
+    public static implicit operator PosterViewModel(Poster p)
+    {
+        PosterViewModel vm = new PosterViewModel
+        {
+            ClubCode = p.ACT_CDE.Trim(),
+            Title = p.Title,
+            Description = p.Description ?? "",
+            ImagePath = p.ImagePath,
+            VisibleDate = p.VisibleDate,
+            ExpirationDate = p.ExpirationDate,
+            Status = p.Status.Status
+        };
+
+        return vm;
+    }
+}
