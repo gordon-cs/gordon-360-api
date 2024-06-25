a7f9650a11df049f556fadb2a549d3a6f3df1e8b
amos-cha <amos.cha98@gmail.com>
2024-06-25 11:20:46 -0400

    Add image update service

--- Gordon360/Services/PosterService.cs
+++ Gordon360/Services/PosterService.cs
@@ -11,6 +11,13 @@ using Gordon360.Extensions.System;
 using Gordon360.Enums;
 using System;
 using Microsoft.EntityFrameworkCore;
+using Gordon360.Utilities;
+using Microsoft.Graph;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph.TermStore;
+
 
 namespace Gordon360.Services;
 
@@ -18,7 +25,11 @@ namespace Gordon360.Services;
 /// <summary>
 /// Service Class that facilitates data transactions between the PosterController and the Poster database model.
 /// </summary>
-public class PosterService(CCTContext context, SessionService sessionService, MembershipService membershipService) : IPosterService
+public class PosterService(CCTContext context, 
+                            SessionService sessionService, 
+                            MembershipService membershipService, 
+                            IWebHostEnvironment webHostEnvironment,
+                            ServerUtils serverUtils) : IPosterService
 {
     public IEnumerable<PosterViewModel> GetPosters() {
         return context.Poster.Include(p => p.Status).Select(p => (PosterViewModel)p);
@@ -62,7 +73,68 @@ public class PosterService(CCTContext context, SessionService sessionService, Me
 
     public async Task<PosterViewModel> UpdatePosterAsync(int posterID, PosterPatchViewModel updatedPoster)
     {
-        return null;
+        var poster = context.Poster.Find(posterID);
+        if (poster == null) throw new ResourceNotFoundException
+        { ExceptionMessage = $"Poster with ID: {posterID} not found" };
+
+        poster.Title = updatedPoster.Title ?? poster.Title;
+        poster.Description = updatedPoster.Description ?? poster.Description;
+        poster.VisibleDate = updatedPoster.VisibleDate ?? poster.VisibleDate;
+        poster.ExpirationDate = updatedPoster.ExpirationDate ?? poster.ExpirationDate;
+
+        if (updatedPoster.Status is not null)
+            poster.StatusID = context.PosterStatus
+               .Where(ps => String.Equals(ps.Status, updatedPoster.Status, StringComparison.CurrentCultureIgnoreCase))
+               .FirstOrDefault()?
+               .ID ?? poster.StatusID;
+
+
+        poster.ImagePath = updatedPoster.ImagePath ?? poster.ImagePath;
+
+        if (updatedPoster.ImagePath is not null)
+        {
+            // ImageUtils.GetImageFormat checks whether the image type is valid (jpg/jpeg/png)
+            var (extension, format, data) = ImageUtils.GetImageFormat(updatedPoster.ImagePath);
+
+            string? imagePath = null;
+
+            if (poster.ImagePath is not null && updatedPoster.ImagePath is null)
+            {
+                imagePath = GetImagePath(Path.GetFileName(poster.ImagePath));
+                ImageUtils.DeleteImage(imagePath);
+                poster.ImagePath = updatedPoster.ImagePath;
+            }
+            else
+            {
+                // Use a unique alphanumeric GUID string as the file name
+                var filename = $"{Guid.NewGuid().ToString("N")}.{extension}";
+                imagePath = GetImagePath(filename);
+                var url = GetImageURL(filename);
+                poster.ImagePath = url;
+                ImageUtils.UploadImage(imagePath, data, format);
+            }
+        }
+
+        await context.SaveChangesAsync();
+
+        return poster;
+    }
+
+    private string GetImagePath(string filename)
+    {
+        return Path.Combine(webHostEnvironment.ContentRootPath, "browseable", "uploads", "recim", "team", filename);
+    }
+
+    private string GetImageURL(string filename)
+    {
+        var serverAddress = serverUtils.GetAddress();
+        if (serverAddress is not string) throw new Exception("Could not upload poster: Server Address is null");
+
+        if (serverAddress.Contains("localhost"))
+            serverAddress += '/';
+        //temporarily using rec-im until we have our own folder
+        var url = $"{serverAddress}browseable/uploads/recim/team/{filename}";
+        return url;
     }
 
     public async Task<PosterViewModel> DeletePosterAsync(int posterID)
