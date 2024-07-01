be9a96218c5e92acf326e7ed1f9b2821013fc691
amos-cha <amos.cha98@gmail.com>
2024-07-01 14:51:08 -0400

    Create TODO

--- Gordon360/Services/RecIM/SeriesService.cs
+++ Gordon360/Services/RecIM/SeriesService.cs
@@ -1681,6 +1681,38 @@ public class SeriesService(CCTContext context, IMatchService matchService, IAffi
 
     public IEnumerable<MatchBracketViewModel> GetSeriesBracketInformation(int seriesID)
     {
+        /**
+         * TODO
+         *   Shape
+                     {
+                        "id": 260005,
+                        "name": "Final - Match",
+                        "nextMatchId": null, // Id for the nextMatch in the bracket, if it's final match it must be null OR undefined
+                        "tournamentRoundText": "4", // Text for Round Header
+                        "startTime": "2021-05-30",
+                        "state": "DONE", // 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | 'DONE' | 'SCORE_DONE' Only needed to decide walkovers and if teamNames are TBD (to be decided)
+                        "participants": [
+                          {
+                            "id": "c016cb2a-fdd9-4c40-a81f-0cc6bdf4b9cc", // Unique identifier of any kind
+                            "resultText": "WON", // Any string works
+                            "isWinner": false,
+                            "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | null
+                            "name": "giacomo123"
+                          },
+                          {
+                            "id": "9ea9ce1a-4794-4553-856c-9a3620c0531b",
+                            "resultText": null,
+                            "isWinner": true,
+                            "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY'
+                            "name": "Ant"
+                          }
+                        ]
+                      }
+          *
+          * 1) Empty matches are not currently made, might have to create dead matches with deleted status
+          * 2) need 'next match ID
+          * 3) status needs to be translated to 'state' enum
+         */
         return context.Match
             .Include(m => m.MatchBracket)
             .Where(m => m.SeriesID == seriesID && m.StatusID != 0)
