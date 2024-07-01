8dc4e8e27db74560b55e69f9e009c3e4fb088550
amos-cha <amos.cha98@gmail.com>
2024-07-01 16:04:06 -0400

    Create MatchBracketExtendedViewModel

---
+++ Gordon360/Models/ViewModels/RecIM/MatchBracketExtendedViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Gordon360.Models.CCT;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Gordon360.Models.ViewModels.RecIM;
+
+public class MatchBracketExtendedViewModel
+{
+    public int MatchID { get; set; }
+    public int? NextMatchID { get; set; }
+    public int RoundNumber { get; set; }
+    public int RoundOf { get; set; }
+    public string? State { get; set; }
+    public IEnumerable<TeamExtendedViewModel> Team { get; set; }
+    public int SeedIndex { get; set; }
+    public bool IsLosers { get; set; }
+}
