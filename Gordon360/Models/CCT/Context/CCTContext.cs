ca542e445d41dd99e19818fd2ece72fd3e2fe4e8
EjPlatzer <Evan.Platzer@gordon.edu>
2024-09-25 11:59:53 -0400

    RecIM: Add Hall to ParticipantView

--- Gordon360/Models/CCT/Context/CCTContext.cs
+++ Gordon360/Models/CCT/Context/CCTContext.cs
@@ -468,6 +468,7 @@ public partial class CCTContext : DbContext
         {
             entity.ToView("ParticipantView", "RecIM");
 
+            entity.Property(e => e.Hall).IsFixedLength();
             entity.Property(e => e.SpecifiedGender).IsFixedLength();
         });
 
