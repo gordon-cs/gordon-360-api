b5fb77ec792ef474d12217320c7620a8f8520bc2
Matthew Jones <Matthew.Jones@gordon.edu>
2024-10-25 20:27:15 -0400

    Added work on post request for missing item reports

---
+++ Gordon360/Models/ViewModels/MissingItemReportViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Gordon360.Models.ViewModels
+{
+    public class MissingItemReportViewModel
+    {
+        public int? recordID { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string category { get; set; }
+
+        public string[]? colors { get; set; }
+
+        public string? brand { get; set; }
+
+        public string description { get; set; }
+
+        public string locationLost { get; set; }
+
+        public bool stolen { get; set; }
+
+        public string? stolenDescription { get; set; }
+
+        public DateTime dateLost { get; set; }
+
+        public DateTime dateCreated { get; set; }
+
+        public string phoneNumber { get; set; }
+
+        public string? altPhone { get; set; }
+
+        public string emailAddress { get; set; }
+
+        public string status { get; set; }
+
+        public string? adminUsername { get; set; }
+    }
+}
--- Gordon360/Services/LostAndFoundService.cs
+++ Gordon360/Services/LostAndFoundService.cs
@@ -1,5 +1,7 @@
-﻿using Gordon360.Models.CCT;
+﻿using Gordon360.Exceptions;
+using Gordon360.Models.CCT;
 using Gordon360.Models.CCT.Context;
+using Gordon360.Models.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,6 +10,25 @@ namespace Gordon360.Services
 {
     public class LostAndFoundService(CCTContext context) : ILostAndFoundService
     {
+        public int CreateMissingItemReport(MissingItemReportViewModel reportDetails)
+        {
+          
+
+            var newReportResults = context.Missing.Add(new Missing { firstName = reportDetails.firstName, lastName = reportDetails.lastName, category = reportDetails.category, brand = reportDetails.brand, description = reportDetails.description, locationLost = reportDetails.locationLost, stolen = reportDetails.stolen, stolenDescription = reportDetails.stolenDescription, dateLost = reportDetails.dateLost, dateCreated = reportDetails.dateCreated, phoneNumber = reportDetails.phoneNumber, altPhone = reportDetails.altPhone, emailAddr = reportDetails.emailAddress, status = reportDetails.status, adminUsername = reportDetails.adminUsername });
+
+            context.SaveChanges();
+
+            if (newReportResults?.Entity == null)
+            {
+                throw new ResourceCreationException() { ExceptionMessage = "The application could not be saved." };
+            }
+
+            // Retrieve the application ID number of this new application
+            int reportID = newReportResults.Entity.recordID;
+
+            return reportID;
+        }
+
         public IEnumerable<Missing> GetMissingItems()
         {
             return context.Missing.AsEnumerable();
