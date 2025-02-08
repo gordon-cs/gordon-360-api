be17e574718aa0059b438e82844f04bfa55d7757
Matthew Jones <Matthew.Jones@gordon.edu>
2025-02-08 14:14:26 -0500

    Fixes from review

--- Gordon360/Services/ServiceInterfaces.cs
+++ Gordon360/Services/ServiceInterfaces.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using RecIMActivityViewModel = Gordon360.Models.ViewModels.RecIM.ActivityViewModel;
