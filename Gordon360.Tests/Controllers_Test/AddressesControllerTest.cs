de23a2ddf761ba0d5807f12af933596e522b3475
Philemon Zhang <Philemon.Zhang@gordon.edu>
2025-06-23 10:14:41 -0400

    AddressControllerTests done

--- Gordon360.Tests/Controllers_Test/AddressesControllerTest.cs
+++ Gordon360.Tests/Controllers_Test/AddressesControllerTest.cs
@@ -36,4 +36,30 @@ public class AddressesControllerTests
             state => Assert.Equal("MA", state.Abbreviation),
             state => Assert.Equal("CA", state.Abbreviation));
     }
+    [Fact]
+    public void GetAllCountries_ReturnsOkWithListOfCountries()
+    {
+        // Arrange
+        var mockService = new Mock<IAddressesService>();
+        var fakeCountries = new List<CountryViewModel>
+        {
+            new CountryViewModel("United States", "US"),
+            new CountryViewModel("Canada", "CA")
+        };
+        // Set up the mocked service to return the fake countries list
+        mockService.Setup(s => s.GetAllCountries()).Returns(fakeCountries);
+        // Create the controller with the mocked service injected
+        var controller = new AddressesController(mockService.Object);
+        // Act
+        var result = controller.GetAllCountries(); // returns ActionResult<IEnumerable<CountryViewModel>>
+        // Assert
+        // 1. Assert the result is 200 OK
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        // 2. Assert the result's Value is a list of CountryViewModel
+        var returnValue = Assert.IsAssignableFrom<IEnumerable<CountryViewModel>>(okResult.Value);
+        // 3. Assert the content of the list is exactly what we expect
+        Assert.Collection(returnValue,
+            country => Assert.Equal("US", country.Abbreviation),
+            country => Assert.Equal("CA", country.Abbreviation));
+    }
 }
