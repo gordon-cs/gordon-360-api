cc08942a6fe9cc933f59f60b80477276f8f6a03f
muchen <philemon.zhamg@gordon.edu>
2025-07-07 10:55:18 -0400

    add test for schedulecontroller and sessionscontroller

---
+++ Gordon360.Tests/Controllers_Test/ScheduleControllerTest.cs
@@ -0,0 +1,196 @@
+using Gordon360.Controllers;
+using Gordon360.Models.ViewModels;
+using Gordon360.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Gordon360.Tests.Controllers_Test;
+
+public class ScheduleControllerTest
+{
+    private readonly Mock<IScheduleService> _mockService;
+    private readonly ScheduleController _controller;
+
+    public ScheduleControllerTest()
+    {
+        _mockService = new Mock<IScheduleService>();
+        _controller = new ScheduleController(_mockService.Object);
+    }
+
+    private void SetUser(string username, string role)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, username),
+            new Claim(ClaimTypes.Role, role)
+        };
+        var identity = new ClaimsIdentity(claims, "TestAuthType");
+        var user = new ClaimsPrincipal(identity);
+        _controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext { User = user }
+        };
+    }
+
+    private static UserCoursesViewModel GetSampleCourse() => new UserCoursesViewModel
+    {
+        SessionCode = "202401",
+        YR_CDE = "2024",
+        TRM_CDE = "SP",
+        CRS_CDE = "CS101",
+        CRS_TITLE = "Intro to CS",
+        BLDG_CDE = "KEN",
+        ROOM_CDE = "101",
+        MONDAY_CDE = "Y",
+        TUESDAY_CDE = "N",
+        WEDNESDAY_CDE = "Y",
+        THURSDAY_CDE = "N",
+        FRIDAY_CDE = "Y",
+        SATURDAY_CDE = "N",
+        BEGIN_TIME = new TimeSpan(9, 0, 0),
+        END_TIME = new TimeSpan(10, 15, 0),
+        BEGIN_DATE = new DateTime(2024, 1, 10),
+        END_DATE = new DateTime(2024, 5, 10),
+        SUB_TERM_CDE = "A",
+        Role = "Student"
+    };
+
+    [Fact]
+    public async Task GetAllCourses_ReturnsOk_ForSelf()
+    {
+        var username = "jdoe";
+        var courses = new List<UserCoursesViewModel> { GetSampleCourse() };
+        var session = new CoursesBySessionViewModel("202401", "Spring 2024", new DateTime(2024, 1, 10), new DateTime(2024, 5, 10), courses);
+        var sessions = new List<CoursesBySessionViewModel> { session };
+        _mockService.Setup(s => s.GetAllCoursesAsync(username)).ReturnsAsync(sessions);
+
+        SetUser(username, "Student");
+
+        var result = await _controller.GetAllCourses(username);
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returned = Assert.IsAssignableFrom<IEnumerable<CoursesBySessionViewModel>>(okResult.Value);
+        Assert.Single(returned);
+        Assert.Equal("202401", returned.First().SessionCode);
+    }
+
+    [Fact]
+    public async Task GetAllCourses_ReturnsOk_ForFacStaff()
+    {
+        var username = "jdoe";
+        var courses = new List<UserCoursesViewModel> { GetSampleCourse() };
+        var session = new CoursesBySessionViewModel("202401", "Spring 2024", new DateTime(2024, 1, 10), new DateTime(2024, 5, 10), courses);
+        var sessions = new List<CoursesBySessionViewModel> { session };
+        _mockService.Setup(s => s.GetAllCoursesAsync(username)).ReturnsAsync(sessions);
+
+        SetUser("facstaff", "FacStaff");
+
+        var result = await _controller.GetAllCourses(username);
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returned = Assert.IsAssignableFrom<IEnumerable<CoursesBySessionViewModel>>(okResult.Value);
+        Assert.Single(returned);
+        Assert.Equal("202401", returned.First().SessionCode);
+    }
+
+    [Fact]
+    public async Task GetAllCourses_ReturnsOk_ForOtherUser()
+    {
+        // This test covers the 'not self, not facstaff' branch
+        var username = "otheruser";
+        var courses = new List<UserCoursesViewModel> { GetSampleCourse() };
+        var session = new CoursesBySessionViewModel("202401", "Spring 2024", new DateTime(2024, 1, 10), new DateTime(2024, 5, 10), courses);
+        var sessions = new List<CoursesBySessionViewModel> { session };
+        _mockService.Setup(s => s.GetAllInstructorCoursesAsync(username)).ReturnsAsync(sessions);
+
+        SetUser("jdoe", "Student");
+
+        var result = await _controller.GetAllCourses(username);
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returned = Assert.IsAssignableFrom<IEnumerable<CoursesBySessionViewModel>>(okResult.Value);
+        Assert.Single(returned);
+        Assert.Equal("202401", returned.First().SessionCode);
+    }
+
+    [Fact]
+    public async Task GetAllCoursesByTerm_ReturnsOk_ForSelf()
+    {
+        var username = "jdoe";
+        var courses = new List<UserCoursesViewModel> { GetSampleCourse() };
+        var term = new CoursesByTermViewModel("2024", "SP", "Spring 2024", new DateTime(2024, 1, 10), new DateTime(2024, 5, 10), courses);
+        var terms = new List<CoursesByTermViewModel> { term };
+        _mockService.Setup(s => s.GetAllCoursesByTermAsync(username)).ReturnsAsync(terms);
+
+        SetUser(username, "Student");
+
+        var result = await _controller.GetAllCoursesByTerm(username);
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returned = Assert.IsAssignableFrom<IEnumerable<CoursesByTermViewModel>>(okResult.Value);
+        Assert.Single(returned);
+        Assert.Equal("2024", returned.First().YearCode);
+    }
+
+    [Fact]
+    public async Task GetAllCoursesByTerm_ReturnsOk_ForFacStaff()
+    {
+        var username = "jdoe";
+        var courses = new List<UserCoursesViewModel> { GetSampleCourse() };
+        var term = new CoursesByTermViewModel("2024", "SP", "Spring 2024", new DateTime(2024, 1, 10), new DateTime(2024, 5, 10), courses);
+        var terms = new List<CoursesByTermViewModel> { term };
+        _mockService.Setup(s => s.GetAllCoursesByTermAsync(username)).ReturnsAsync(terms);
+
+        SetUser("facstaff", "FacStaff");
+
+        var result = await _controller.GetAllCoursesByTerm(username);
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returned = Assert.IsAssignableFrom<IEnumerable<CoursesByTermViewModel>>(okResult.Value);
+        Assert.Single(returned);
+        Assert.Equal("2024", returned.First().YearCode);
+    }
+
+    [Fact]
+    public async Task GetAllCoursesByTerm_ReturnsOk_ForOtherUser()
+    {
+        // This test covers the 'not self, not facstaff' branch
+        var username = "otheruser";
+        SetUser("jdoe", "Student");
+
+        var result = await _controller.GetAllCoursesByTerm(username);
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returned = Assert.IsAssignableFrom<IEnumerable<CoursesByTermViewModel>>(okResult.Value);
+        Assert.Empty(returned);
+    }
+
+    [Fact]
+    public async Task GetCanReadStudentSchedules_ReturnsTrue_ForAdvisor()
+    {
+        SetUser("advisor", "Advisors");
+
+        var result = await _controller.GetCanReadStudentSchedules();
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.True((bool)okResult.Value);
+    }
+
+    [Fact]
+    public async Task GetCanReadStudentSchedules_ReturnsFalse_ForNonAdvisor()
+    {
+        SetUser("jdoe", "Student");
+
+        var result = await _controller.GetCanReadStudentSchedules();
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.False((bool)okResult.Value);
+    }
+} 
---
+++ Gordon360.Tests/Controllers_Test/SessionsControllerTest.cs
@@ -0,0 +1,139 @@
+using Gordon360.Controllers;
+using Gordon360.Models.ViewModels;
+using Gordon360.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Gordon360.Tests.Controllers_Test;
+
+public class SessionsControllerTest
+{
+    private readonly Mock<ISessionService> _mockService;
+    private readonly SessionsController _controller;
+
+    public SessionsControllerTest()
+    {
+        _mockService = new Mock<ISessionService>();
+        _controller = new SessionsController(_mockService.Object);
+    }
+
+    private static SessionViewModel GetSampleSession()
+    {
+        return new SessionViewModel
+        {
+            SessionCode = "202401",
+            SessionDescription = "Spring 2024",
+            SessionBeginDate = new DateTime(2024, 1, 10),
+            SessionEndDate = new DateTime(2024, 5, 10)
+        };
+    }
+
+    [Fact]
+    public void Get_ReturnsOk_WithList()
+    {
+        var sessions = new List<SessionViewModel> { GetSampleSession() };
+        _mockService.Setup(s => s.GetAll()).Returns(sessions);
+
+        var result = _controller.Get();
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returned = Assert.IsAssignableFrom<IEnumerable<SessionViewModel>>(okResult.Value);
+        Assert.Single(returned);
+        Assert.Equal("202401", returned.First().SessionCode);
+    }
+
+    [Fact]
+    public void Get_ReturnsOk_WithEmptyList()
+    {
+        _mockService.Setup(s => s.GetAll()).Returns(new List<SessionViewModel>());
+
+        var result = _controller.Get();
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returned = Assert.IsAssignableFrom<IEnumerable<SessionViewModel>>(okResult.Value);
+        Assert.Empty(returned);
+    }
+
+    [Fact]
+    public void Get_ById_ReturnsOk_WhenSessionExists()
+    {
+        var session = GetSampleSession();
+        _mockService.Setup(s => s.Get("202401")).Returns(session);
+
+        var result = _controller.Get("202401");
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returned = Assert.IsType<SessionViewModel>(okResult.Value);
+        Assert.Equal("202401", returned.SessionCode);
+    }
+
+    [Fact]
+    public void Get_ById_ReturnsNotFound_WhenSessionDoesNotExist()
+    {
+        _mockService.Setup(s => s.Get("notfound")).Returns((SessionViewModel)null);
+
+        var result = _controller.Get("notfound");
+
+        Assert.IsType<NotFoundResult>(result.Result);
+    }
+
+    [Fact]
+    public void GetCurrentSession_ReturnsOk_WhenSessionExists()
+    {
+        var session = GetSampleSession();
+        _mockService.Setup(s => s.GetCurrentSession()).Returns(session);
+
+        var result = _controller.GetCurrentSession();
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returned = Assert.IsType<SessionViewModel>(okResult.Value);
+        Assert.Equal("202401", returned.SessionCode);
+    }
+
+    [Fact]
+    public void GetCurrentSession_ReturnsNotFound_WhenNoSession()
+    {
+        _mockService.Setup(s => s.GetCurrentSession()).Returns((SessionViewModel)null);
+
+        var result = _controller.GetCurrentSession();
+
+        Assert.IsType<NotFoundResult>(result.Result);
+    }
+
+    [Fact]
+    public void GetDaysLeftInSemester_ReturnsOk_WithValidDays()
+    {
+        var days = new double[] { 30, 120 };
+        _mockService.Setup(s => s.GetDaysLeft()).Returns(days);
+
+        var result = _controller.GetDaysLeftInSemester();
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returned = Assert.IsType<double[]>(okResult.Value);
+        Assert.Equal(30, returned[0]);
+        Assert.Equal(120, returned[1]);
+    }
+
+    [Fact]
+    public void GetDaysLeftInSemester_ReturnsNotFound_WhenNoDays()
+    {
+        _mockService.Setup(s => s.GetDaysLeft()).Returns((double[])null);
+
+        var result = _controller.GetDaysLeftInSemester();
+
+        Assert.IsType<NotFoundResult>(result.Result);
+    }
+
+    [Fact]
+    public void GetDaysLeftInSemester_ReturnsNotFound_WhenZeroDays()
+    {
+        _mockService.Setup(s => s.GetDaysLeft()).Returns(new double[] { 0, 0 });
+
+        var result = _controller.GetDaysLeftInSemester();
+
+        Assert.IsType<NotFoundResult>(result.Result);
+    }
+} 
