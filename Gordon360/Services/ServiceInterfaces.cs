f28998f365b00710ce36bd30e73fb4b11ed2b234
Josh Miller <josh.miller@gordon.edu>
2025-07-18 11:29:33 -0400

    Change getCurrentPosters logic to return priority first, then posters the user is involved in, and then all other posters

--- Gordon360/Services/ServiceInterfaces.cs
+++ Gordon360/Services/ServiceInterfaces.cs
@@ -340,7 +340,7 @@ namespace Gordon360.Services
     public interface IPosterService
     {
         IEnumerable<PosterViewModel> GetPosters();
-        IEnumerable<PosterViewModel> GetCurrentPosters();
+        IEnumerable<PosterViewModel> GetCurrentPosters(string username);
         IEnumerable<PosterViewModel> GetCurrentPostersByActivityCode(string activityCode);
         IEnumerable<PosterViewModel> GetPersonalizedPostersByUsername(string username);
         IEnumerable<string> GetPosterStatuses();
