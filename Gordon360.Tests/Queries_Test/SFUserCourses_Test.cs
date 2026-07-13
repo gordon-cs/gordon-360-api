1339ddd29dc2003d633b14672c46c12162012a36
Levi D Lacoss <Levi.Lacoss@gordon.edu>
2026-07-13 15:20:17 -0400

    remove unneeded files

--- Gordon360.Tests/Queries_Test/SFUserCourses_Test.cs
+++
@@ -1,8 +0,0 @@
-using Gordon360.Models.Salesforce;
-
-namespace Gordon360.Tests.Queries_Test;
-
-public class SFUserCourses_Test
-{
-    Mock<ISalesforceContext> mockService = new Mock<ISalesforceContext>();    
-}
